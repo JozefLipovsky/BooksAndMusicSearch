177b2d2433e42481015cde113c0b33b1cebd3b1e
Jozef Lipovsky <j.lipovsky@me.com>
2016-08-11 20:03:11 +0200

    Add basic table view data source setup

---
+++ BooksAndMusicSearch/BooksAndMusicSearch/ViewControllers/MainViewController.swift
@@ -0,0 +1,85 @@
+//
+//  MainViewController.swift
+//  BooksAndMusicSearch
+//
+//  Created by JoLi on 2016-08-11.
+//  Copyright © 2016 JoLi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let kMusicTableSection = 0
+    let kBooksTableSection = 1
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - TableView Data Source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case kMusicTableSection:
+            // music data source count
+            return 2
+        case kBooksTableSection:
+            // music data source count
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell!
+        
+        switch indexPath.section {
+        case kMusicTableSection:
+            cell = tableView.dequeueReusableCellWithIdentifier("MusicCell", forIndexPath: indexPath) as UITableViewCell
+            
+        case kBooksTableSection:
+            cell = tableView.dequeueReusableCellWithIdentifier("BookCell", forIndexPath: indexPath) as UITableViewCell
+            
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case kMusicTableSection:
+            return "Music"
+        case kBooksTableSection:
+            return "Books"
+        default:
+            return nil
+        }
+    }
+    
+    
+    
+}
